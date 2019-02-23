dffb3bb9ea900bad3289d4f89942fb468d421fd8
Raj T <rnamboodiri@gmail.com>
2019-02-23 21:31:39 +0000

    How to create a VPC and then a K8S cluster

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # Infrastructure as Code for Google Cloud Platform Using Terraform
-How to create GCP infrastructure using Hashicorp Terraform
+How to create GCP infrastructure using [Hashicorp Terraform](https://www.terraform.io/). This repository contains simple set of Terraform scripts to create individual GCP services. It is also important to note that, this is not following any **best practices** of writing Terraform code. The idea here is to make things work for the first time and improvise based on your needs. For all of the examples to work, the following pre-requisites are required.
+* A valid GCP account
+* The key of the user/service account got downloaded into your local machine
+* Terraform is installed and configured (Terraform v0.11.11 is being used here)
+* All the examples are run from a Mac machine
+
+When a security key is downloaded, keep it in a local directory and export the location in an environment variable. Make sure that the key is removed as and when your needs are over to prevent from your account being compromised. Key rotation is also a good method.
+```bash
+export GOOGLE_CLOUD_KEYFILE_JSON=‎⁨‎⁨‎⁨‎⁨/Users/RajT/workspace/gcp-terraform/keys/infra.json
+echo $GOOGLE_CLOUD_KEYFILE_JSON
+```
+When you go to each of the directories, make sure that you edit the scripts to include the following parameters.
+* Your GCP project ID
+* Your preferred GCP region
+* Your preferred GCP zone
+
+Once you are done with all these steps given above, use the following Terraform commands
+```bash
+# Make the initialization
+terraform init
+# See the plan of action
+terraform plan
+# Create the infrastructure
+terraform apply
+# Once all the work is completed, destroy the infrastructure that you have created
+terraform destroy
+```
+
+## List of Scripts Directories
+The following directories contain Terraform scripts. Some of the scripts are dependent on the scripts in the other directories. Wherever there are dependencies like this, it is called out with **DEPENDENCIES**
+* vpc - This contains Terraform scripts to create a VPC with subnet for creating your GCP service resources
+* k8s-cluster - This contains Terraform scripts to create a Kubernetes (K8S) cluster with Istio enabled. **DEPENDENCIES** - The VPC created using the scripts in the directory **vpc** is used to create the K8S cluster.
+
+## References
+* Managing GCP Projects with Terraform - https://cloud.google.com/community/tutorials/managing-gcp-projects-with-terraform
+* Understanding GCP Roles - https://cloud.google.com/iam/docs/understanding-roles
+* Using Terraform with Makefiles - http://saurabh-hirani.github.io/writing/2017/08/02/terraform-makefile
---
+++ k8s-cluster/main.tf
@@ -0,0 +1,115 @@
+variable "gcp_project" {
+  type    = "string"
+  default = "protean-1217618"
+}
+variable "gcp_region" {
+  type    = "string"
+  default = "europe-west2"
+}
+variable "gcp_zone" {
+  type    = "string"
+  default = "europe-west2-a"
+}
+variable "gcp_network" {
+  type    = "string"
+  default = "mservice-network"
+}
+variable "gcp_subnetwork" {
+  type    = "string"
+  default = "mservice-subnetwork"
+}
+
+
+
+provider "google" {
+  project = "${var.gcp_project}"
+  region  = "${var.gcp_region}"
+  zone    = "${var.gcp_zone}"
+}
+provider "google-beta" {
+  project = "${var.gcp_project}"
+  region  = "${var.gcp_region}"
+  zone    = "${var.gcp_zone}"
+}
+resource "google_container_cluster" "mservice" {
+  provider = "google-beta"
+  name      = "mservice-dev-cluster"
+  region    = "${var.gcp_region}"
+  network   = "${var.gcp_network}"
+  subnetwork = "${var.gcp_subnetwork}"
+
+
+  # We can't create a cluster with no node pool defined, but we want to only use
+  # separately managed node pools. So we create the smallest possible default
+  # node pool and immediately delete it.
+  remove_default_node_pool = true
+  initial_node_count = 1
+
+  # Setting an empty username and password explicitly disables basic auth
+  master_auth {
+    username = ""
+    password = ""
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels = {
+      foo = "mservice"
+    }
+
+    tags = ["owner", "rajt"]
+  }
+  addons_config {
+    http_load_balancing {
+      disabled = true
+    }
+    horizontal_pod_autoscaling {
+      disabled = true
+    }
+    istio_config {
+      disabled = false
+    }
+    kubernetes_dashboard {
+      disabled = false
+    }
+  }
+}
+
+resource "google_container_node_pool" "mservice_preemptible_nodes" {
+  name       = "mservice-node-pool"
+  region     = "${var.gcp_region}"
+  cluster    = "${google_container_cluster.mservice.name}"
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "n1-standard-1"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
+
+# The following outputs allow authentication and connectivity to the GKE Cluster
+# by using certificate-based authentication.
+output "client_certificate" {
+  value = "${google_container_cluster.mservice.master_auth.0.client_certificate}"
+}
+
+output "client_key" {
+  value = "${google_container_cluster.mservice.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.mservice.master_auth.0.cluster_ca_certificate}"
+}
