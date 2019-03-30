3a31db741b57d5b31afd39f4653e5e18861494ac
Raj T <rnamboodiri@gmail.com>
2019-03-30 09:03:49 +0000

    Fixed the role binding issue

--- iam/main.tf
+++ iam/main.tf
@@ -18,3 +18,10 @@ resource "google_project_iam_custom_role" "mservice_infra_admin" {
   description = "Infrastructure Administrator Custom Role"
   permissions = ["compute.disks.create", "compute.firewalls.create", "compute.firewalls.delete", "compute.firewalls.get", "compute.instanceGroupManagers.get", "compute.instances.create", "compute.instances.delete", "compute.instances.get", "compute.instances.setMetadata", "compute.instances.setServiceAccount", "compute.instances.setTags", "compute.machineTypes.get", "compute.networks.create", "compute.networks.delete", "compute.networks.get", "compute.networks.updatePolicy", "compute.subnetworks.create", "compute.subnetworks.delete", "compute.subnetworks.get", "compute.subnetworks.setPrivateIpGoogleAccess", "compute.subnetworks.update", "compute.subnetworks.use", "compute.subnetworks.useExternalIp", "compute.zones.get", "container.clusters.create", "container.clusters.delete", "container.clusters.get", "container.clusters.update", "container.operations.get"]
 }
+resource "google_project_iam_binding" "mservice_infra_binding" {
+  role = "projects/${var.gcp_project}/roles/${google_project_iam_custom_role.mservice_infra_admin.role_id}"
+
+  members = [
+    "serviceAccount:${google_service_account.mservice_infra_service_account.email}",
+  ]
+}
